11cd039e9d4ddc3a3b9b0a76d318d48de2db8bda
Nikunj Kumar <nikunjkumar6379@gmail.com>
2025-02-04 13:55:25 +0530

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Home() {
+  return (
+    <section className='py-24'>
+      <div className='container'>
+        <h1 className='text-xl font-bold text-center'>
+          Next.js Template File
+          </h1>
+      </div>
+    </section>
+  )
+}
